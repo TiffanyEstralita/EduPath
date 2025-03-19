78eabb91631871f744983722d6c4d4e504c32f2c
TiffanyEstralita <164315549+TiffanyEstralita@users.noreply.github.com>
2025-03-19 14:59:35 +0800

    Delete edupath directory

--- edupath/config/datasets.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-	'Singapore Polytechnic' => 'https://data.gov.sg/api/action/datastore_search?resource_id=d_aa3b89617725a58af2587ccea25e6950',
-	'Temasek Polytechnic' => 'https://data.gov.sg/api/action/datastore_search?resource_id=d_b438fc00ee679a15980282fe1c7ca45d',
-	'Nanyang Technological University' => 'https://data.gov.sg/api/action/datastore_search?resource_id=d_82988b8b68c649ad9e39f115559380c2',
-	'National University of Singapore' => 'https://data.gov.sg/api/action/datastore_search?resource_id=d_354c65ac58924c0b85013c2c3549e5a9',
-	'Singapore University of Technology and Design' => 'https://data.gov.sg/api/action/datastore_search?resource_id=d_50e8fc2285374050a5a74383e0e2c83e',
-];
